01cd9c8e2abd79ed7d89cae03afd5dcbe222d5cd
Claudio <claudiocarchietti@yahoo.it>
2021-03-22 16:41:41 +0100

    Inizio del bonus

--- icons/dati.js
+++ icons/dati.js
@@ -1,109 +1,142 @@
-const icons = [
-	{
-		name: 'cat',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'crow',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dog',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dove',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dragon',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'horse',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'hippo',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'fish',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'carrot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'apple-alt',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'lemon',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'pepper-hot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'user-astronaut',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-graduate',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-ninja',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-secret',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	}
-];
+$(document).ready(function() {
+	const icons = [
+		{
+			name: 'cat',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'crow',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'dog',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'dove',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'dragon',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'horse',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'hippo',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'fish',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'carrot',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'apple-alt',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'lemon',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'pepper-hot',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'user-astronaut',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		},
+		{
+			name: 'user-graduate',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		},
+		{
+			name: 'user-ninja',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		},
+		{
+			name: 'user-secret',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		}
+	];
 
-icons.forEach((item) => {
-	$('main').append(`
-		<div class="box">
+	//ciclo for each
+	icons.forEach((icons,index) => {
+		//stampo in html le icone
+		$('main').append(`
+			<div class="box">
 			<div class="box-icon">
-				<i class="${item.family} ${item.prefix}${item.name}"></i>
-				<p>${item.name}</p>
+			<i class="${icons.family} ${icons.prefix}${icons.name}"></i>
+			<p>${icons.name}</p>
 			</div>
-		</div>
-		`);
-})
+			</div>
+			`);
+
+			//condizione if con l'index per filtrare specificatamente i colori
+		$($("main i")[index]).css("color", () => {
+			if (icons.type == "animal") {
+				console.log("animal");
+				return "blue"
+			} else if (icons.type == "vegetable") {
+				console.log("vegetable");
+				return "orange"
+			} else if (icons.type == "user") {
+				console.log("user");
+				return "purple"
+			}
+		})
+
+
+		})
+
+}) //fine document
+
+
+
+
+
+// if (icons.type == 'animal') {
+// 	$('i').css("color","blue");
+// }else if (icons.type == 'vegetable') {
+// 	$('i').css("color","orange");
+// } else {
+// 	$('i').css("color","purple");
+// }
